a70d178c0f910786b729ebcdde07599eceab64ae
Andrei Dodu <andrei.dodu@hotmail.it>
2023-01-27 21:49:49 +0100

    fix: code review - text formatting

--- src/main/java/org/isf/accounting/gui/BillDataLoader.java
+++ src/main/java/org/isf/accounting/gui/BillDataLoader.java
@@ -50,15 +50,15 @@ public class BillDataLoader {
 		List<Bill> tableArray = new ArrayList<>();
 
 		switch (status) {
-			case "O":
-				tableArray = getPendingBills(status, username);
-				break;
-			case "ALL":
-				tableArray = getAllBills(username);
-				break;
-			case "C":
-				tableArray = getClosedBills(status, username);
-				break;
+		case "O":
+			tableArray = getPendingBills(status, username);
+			break;
+		case "ALL":
+			tableArray = getAllBills(username);
+			break;
+		case "C":
+			tableArray = getClosedBills(status, username);
+			break;
 		}
 
 		tableArray.sort(Collections.reverseOrder());
@@ -68,42 +68,36 @@ public class BillDataLoader {
 	private List<Bill> getAllBills(String username) {
 		List<Bill> billAll = mergeBillsFromPeriodAndFromPayments();
 		if (username != null) {
-			billAll = billAll.stream().filter(bill-> bill.getUser().equals(username)).collect(Collectors.toList());
+			billAll = billAll.stream().filter(bill -> bill.getUser().equals(username)).collect(Collectors.toList());
 		}
 		Collections.sort(billAll);
 		return billAll;
 	}
 
 	private List<Bill> mergeBillsFromPeriodAndFromPayments() {
-		Map<Integer, Bill> billsSortedById = billPeriod.stream()
-				.collect(Collectors.toMap(Bill::getId, bill -> bill, (a, b) -> b));
+		Map<Integer, Bill> billsSortedById = billPeriod.stream().collect(Collectors.toMap(Bill::getId, bill -> bill, (a, b) -> b));
 		List<Bill> billAll = new ArrayList<>(billPeriod);
-		billFromPayments.stream()
-				.filter(bill -> billsSortedById.get(bill.getId()) == null)
-				.forEach(billAll::add);
+		billFromPayments.stream().filter(bill -> billsSortedById.get(bill.getId()) == null).forEach(billAll::add);
 		return billAll;
 	}
 
 	private List<Bill> getClosedBills(String status, String username) {
-		List<Bill> 	list = billPeriod.stream()
-				.filter(bill -> bill.getStatus().equals(status))
-				.collect(Collectors.toList());
+		List<Bill> list = billPeriod.stream().filter(bill -> bill.getStatus().equals(status)).collect(Collectors.toList());
 		if (username != null) {
-			list = list.stream().filter(bill-> bill.getUser().equals(username)).collect(Collectors.toList());
+			list = list.stream().filter(bill -> bill.getUser().equals(username)).collect(Collectors.toList());
 		}
 		return list;
 	}
 
 	private List<Bill> getPendingBills(String status, String username) throws OHServiceException {
-		if (patientParent != null) return  billManager.getPendingBillsAffiliate(patientParent.getCode()) ; 
-			List<Bill> list = billPeriod.stream()
-						.filter(bill -> bill.getStatus().equals(status))
-						.collect(Collectors.toList());
-			if (username != null) {
-				list = list.stream().filter(bill-> bill.getUser().equals(username)).collect(Collectors.toList());
-			}
-			return list;
-		
+		if (patientParent != null)
+			return billManager.getPendingBillsAffiliate(patientParent.getCode());
+		List<Bill> list = billPeriod.stream().filter(bill -> bill.getStatus().equals(status)).collect(Collectors.toList());
+		if (username != null) {
+			list = list.stream().filter(bill -> bill.getUser().equals(username)).collect(Collectors.toList());
+		}
+		return list;
+
 	}
 
 }
